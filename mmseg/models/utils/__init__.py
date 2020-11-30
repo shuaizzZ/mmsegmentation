0a19e314758d2f9c9d6b3b1478021e712b154519
zhangshuai <zhangshuai@ainnovation.com>
2020-11-30 14:13:34 +0800

    add Mix2Pooling

--- mmseg/models/utils/__init__.py
+++ mmseg/models/utils/__init__.py
@@ -4,8 +4,9 @@ from .res_layer import ResLayer
 from .self_attention_block import SelfAttentionBlock
 from .up_conv_block import UpConvBlock
 from .dupsample_block import DUpsamplingBlock
+from .custom_blocks import Mix2Pooling
 
 __all__ = [
     'ResLayer', 'SelfAttentionBlock', 'make_divisible', 'InvertedResidual',
-    'UpConvBlock', 'DUpsamplingBlock'
+    'UpConvBlock', 'DUpsamplingBlock', 'Mix2Pooling'
 ]
