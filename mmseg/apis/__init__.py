d81f568a8c942271fd35dfe05c8faf9147c210f8
zhangshuai <zhangshuai@ainnovation.com>
2020-11-30 13:56:31 +0800

    add pytoch2onnx to mmseg api

--- mmseg/apis/__init__.py
+++ mmseg/apis/__init__.py
@@ -1,6 +1,7 @@
 from .inference import inference_segmentor, init_segmentor, show_result_pyplot
 from .test import multi_gpu_test, single_gpu_test, mv_single_gpu_test
 from .train import get_root_logger, set_random_seed, train_segmentor
+from .pytoch2onnx import _convert_batchnorm, _demo_mm_inputs, pytorch2onnx
 
 __all__ = [
     'get_root_logger', 'set_random_seed', 'train_segmentor', 'init_segmentor',
