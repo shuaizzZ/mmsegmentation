1217e809976c40ff29d7d190f02d61f1a8aafdba
zhangshuai <zhangshuai@ainnovation.com>
2020-12-02 19:25:26 +0800

    pretrained_models

---
+++ mmseg/models/pretrained_models/__init__.py
@@ -0,0 +1,12 @@
+import os
+import os.path as osp
+
+pretrained_models = {'resnet18': ['resnet', 'resnet18-5c106cde.pth']}
+
+def get_pretrained_path(model_name='resnet18'):
+    pretrained_dir = os.path.split(__file__)[0]
+    model_dir = pretrained_models.get(model_name, None)
+    if model_dir is None:
+        raise ValueError('model_name {} not in '.format(model_name), pretrained_models)
+    pretrained_path = osp.join(pretrained_dir, *model_dir)
+    return pretrained_path
