d4615464b3f4aa8b02acb9247b40594cffe7d009
zhangshuai <zhangshuai@ainnovation.com>
2020-12-15 14:48:17 +0800

    test cfg of ainno

--- configs/pspnet/dupsp_r18_ainno.py
+++ configs/pspnet/dupsp_r18_ainno.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/du_pspnet_r18-d8.py', '../_base_/datasets/ainno_dataset.py',
-    '../_base_/runtimes/yantai_runtime.py', '../_base_/schedules/schedule_yantai.py'
+    '../_base_/runtimes/ainno_runtime.py', '../_base_/schedules/schedule_ainno.py'
 ]
 model = dict(
     decode_head=dict(num_classes=2), auxiliary_head=dict(num_classes=2))
