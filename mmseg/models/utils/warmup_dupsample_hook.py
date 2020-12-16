2d8a99b18a3b400559e9320392aebf61f6a3d70f
zhangshuai <zhangshuai@ainnovation.com>
2020-12-16 17:53:49 +0800

    add bar in duwarmup

--- mmseg/models/utils/warmup_dupsample_hook.py
+++ mmseg/models/utils/warmup_dupsample_hook.py
@@ -10,7 +10,9 @@ import torch.nn as nn
 import torch.distributed as dist
 from torch.utils.data import DataLoader
 
-from mmcv.runner import Hook, build_optimizer
+import mmcv
+from mmcv.runner import Hook, build_optimizer, get_dist_info
+from mmcv.runner.dist_utils import master_only
 from mmcv.runner.iter_based_runner import IterLoader
 from mmseg.datasets.builder import build_dataloader, build_dataset
 from mmseg.models.utils.dupsample_block import DUpsamplingBlock, MirrorDUpsamplingBlock
@@ -39,15 +41,15 @@ class WarmUpDUpsampleHook(Hook):
         self.warmup_du_infos = []
         self.distributed = distributed
         self.runstate = runstate
-
         if 0 == len(self.dupsampleblock_list):
             return
 
         self.interval = cfg.warmup_du_cfg.interval
         self.by_epoch = cfg.warmup_du_cfg.by_epoch
         self._max_runs = cfg.warmup_du_cfg.total_runs
-        self._iter = 0
-        self._epoch = 0
+        self.log_head = 'Epoch' if self.by_epoch else 'Iter'
+        self._iter = 1
+        self._epoch = 1
 
         # build dataset
         dataset = build_dataset(cfg.data.train)
@@ -61,14 +63,14 @@ class WarmUpDUpsampleHook(Hook):
             dist=distributed,
             seed=cfg.seed,
             drop_last=True)
+        self.task_length = len(self.data_loader) if self.by_epoch else self._max_runs
 
         for dupsampleblock in self.dupsampleblock_list:
             warmup_du_model = MirrorDUpsamplingBlock(dupsampleblock)
             optimizer = build_optimizer(warmup_du_model, cfg.warmup_du_cfg.optimizer)
             # put model on gpus
             warmup_du_model = parallel_model(warmup_du_model, cfg.gpu_ids, distributed)
-
-            warmup_du_info = {'model': warmup_du_model, 'optimizer': optimizer, 'du_loss': 0.0}
+            warmup_du_info = {'model': warmup_du_model, 'optimizer': optimizer, 'du_loss': []}
             self.warmup_du_infos.append(warmup_du_info)
 
     def _find_dupsampleblocks(self, model):
@@ -95,30 +97,51 @@ class WarmUpDUpsampleHook(Hook):
                 rec_loss.backward()
             warmup_du_info['optimizer'].step()
             if self.distributed:
-                warmup_du_info['du_loss'] += reduce_loss_for_dist(rec_loss).item()
+                warmup_du_info['du_loss'].append(reduce_loss_for_dist(rec_loss).item())
             else:
-                warmup_du_info['du_loss'] += rec_loss.item()
+                warmup_du_info['du_loss'].append(rec_loss.item())
+        self._print_train_progressbar()
+
+    @master_only
+    def _print_train_progressbar(self, ):
+        rank, world_size = get_dist_info()
+        if self._iter == 1:
+            self.prog_bar = mmcv.ProgressBar(self.task_length * world_size)
+            [self.prog_bar.update() for i in range(world_size)]
+        elif self._iter == self.task_length:
+            [self.prog_bar.update() for i in range(world_size)]
+            self.prog_bar.file.write('\n')
+        else:
+            [self.prog_bar.update() for i in range(world_size)]
+
+    def _print_warmup_infos(self, runner, cur_iter):
+        log_str = f'{self.log_head} [{cur_iter}/{self._max_runs}]\t'
+        for warmup_du_info in self.warmup_du_infos:
+            log_str += 'Du_loss: {} '.format(np.mean(warmup_du_info['du_loss']))
+            warmup_du_info['du_loss'] = []
+        runner.logger.info(log_str)
 
     def _run_epochs(self, runner):
         data_loader = self.data_loader
 
-        while self._epoch < self._max_runs:
-            tbar = tqdm(data_loader, unit='batch', desc='==>DU_WarmUp', ncols=80)
-            for i, data_batch in enumerate(tbar):
+        while self._epoch <= self._max_runs:
+            for i, data_batch in enumerate(data_loader):
+                self._iter = i+1
                 self._run_one_batch(data_batch)
 
-            log_str = f'Epoch [{self._epoch + 1}/{self._max_runs}]\t'
-            for warmup_du_info in self.warmup_du_infos:
-                log_str += 'Du_loss: {} '.format(warmup_du_info['du_loss'] / (i + 1))
-                warmup_du_info['du_loss'] = 0
-            runner.logger.info(log_str)
+            self._print_warmup_infos(runner, self._epoch)
+            # log_str = f'Epoch [{self._epoch + 1}/{self._max_runs}]\t'
+            # for warmup_du_info in self.warmup_du_infos:
+            #     log_str += 'Du_loss: {} '.format(warmup_du_info['du_loss'] / (i + 1))
+            #     warmup_du_info['du_loss'] = 0
+            # runner.logger.info(log_str)
 
             self._epoch = self._epoch + 1
 
     def _run_iters(self, runner):
         iter_loader = IterLoader(self.data_loader)
 
-        while self._iter < self._max_runs:
+        while self._iter <= self._max_runs:
             data_batch = next(iter_loader)
             # w_before1 = self.dupsampleblock_list[0].conv_w.weight.clone()
             # w_before2 = self.model.module.decode_head.dupsample.conv_w.weight.clone()
@@ -128,13 +151,7 @@ class WarmUpDUpsampleHook(Hook):
             # diff1 = torch.sum(w_after1 - w_before1) # verfy conv_w has been optimized
             # diff2 = torch.sum(w_after2 - w_before2)  # verfy conv_w has been optimized
 
-            if (self._iter + 1) % self.interval == 0:
-                log_str = f'Iter [{self._iter + 1}/{self._max_runs}]\t'
-                for warmup_du_info in self.warmup_du_infos:
-                    log_str += 'Du_loss: {} '.format(warmup_du_info['du_loss']/self.interval)
-                    warmup_du_info['du_loss'] = 0
-                runner.logger.info(log_str)
-
+            self._print_warmup_infos(runner, self._iter)
             self._iter = self._iter + 1
 
     def _destroy_resources(self):
