1f397c241986c0daff32425375908cc5b2fc3fa5
zhangshuai <zhangshuai@ainnovation.com>
2020-12-17 17:48:27 +0800

    add default num_classes

--- configs/pspnet/dupsp_r18_ainno.py
+++ configs/pspnet/dupsp_r18_ainno.py
@@ -2,6 +2,7 @@ _base_ = [
     '../_base_/models/du_pspnet_r18-d8.py', '../_base_/datasets/ainno_dataset.py',
     '../_base_/runtimes/ainno_runtime.py', '../_base_/schedules/schedule_ainno.py'
 ]
+num_classes = 2
 model = dict(
-    decode_head=dict(num_classes=2), auxiliary_head=dict(num_classes=2))
+    decode_head=dict(num_classes=num_classes), auxiliary_head=dict(num_classes=num_classes))
 test_cfg = dict(mode='whole')
--- configs/pspnet/dupsp_r18_yantai_st12.py
+++ configs/pspnet/dupsp_r18_yantai_st12.py
@@ -2,6 +2,7 @@ _base_ = [
     '../_base_/models/du_pspnet_r18-d8.py', '../_base_/datasets/yantai_st12.py',
     '../_base_/runtimes/yantai_runtime.py', '../_base_/schedules/schedule_yantai.py'
 ]
+num_classes = 4
 model = dict(
-    decode_head=dict(num_classes=4), auxiliary_head=dict(num_classes=4))
+    decode_head=dict(num_classes=num_classes), auxiliary_head=dict(num_classes=num_classes))
 test_cfg = dict(mode='whole')
