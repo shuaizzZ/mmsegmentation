89ea93fbb63453db971c79a21a17292a34edd3f1
zhangshuai <zhangshuai@ainnovation.com>
2020-12-23 19:08:38 +0800

    paramwise_cfg should be set in optimizer

--- configs/_base_/schedules/schedule_ainno.py
+++ configs/_base_/schedules/schedule_ainno.py
@@ -1,7 +1,7 @@
 # optimizer
 # optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
-optimizer = dict(type='Adamax', lr=0.01, weight_decay=0.0005)
-paramwise_cfg = dict(custom_keys={'.backbone': dict(lr_mult=0.1, decay_mult=0.9)})
+optimizer = dict(type='Ranger', lr=0.01, weight_decay=0.0005,
+                 paramwise_cfg = dict(custom_keys={'backbone': dict(lr_mult=0.1)})) # , decay_mult=0.9
 optimizer_config = dict()
 # learning policy
 # lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
