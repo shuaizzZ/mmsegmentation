8988acdf6551f76697a2838df497dd964595e9be
zhangshuai <zhangshuai@ainnovation.com>
2020-12-28 16:43:34 +0800

    add TPA attention block

--- mmseg/models/utils/__init__.py
+++ mmseg/models/utils/__init__.py
@@ -1,18 +1,19 @@
 from .inverted_residual import InvertedResidual
 from .make_divisible import make_divisible
-from .res_layer import ResLayer
+from .res_layer import ResLayer, ResLayerZH
 from .self_attention_block import SelfAttentionBlock
 from .up_conv_block import UpConvBlock
 
 from .dupsample_block import DUpsamplingBlock, MirrorDUpsamplingBlock
 from .warmup_dupsample_hook import WarmUpDUpsampleHook
 from .custom_blocks import int_size, parallel_model, Mix2Pooling
-from .mv_attention_blocks import SE, SCSE, SCSE2, NonLocal, CBAM, MCBAM, EMA, ECA, AECA
+from .mv_attention_blocks import SE, SCSE, SCSE2, NonLocal, CBAM, MCBAM, EMA, ECA, AECA, TPA
 
 __all__ = [
     'ResLayer', 'SelfAttentionBlock', 'make_divisible', 'InvertedResidual',
     'UpConvBlock',
     'DUpsamplingBlock', 'MirrorDUpsamplingBlock', 'WarmUpDUpsampleHook',
     'int_size', 'parallel_model','Mix2Pooling',
-    'SE', 'SCSE', 'SCSE2', 'NonLocal', 'CBAM', 'MCBAM', 'EMA', 'ECA', 'AECA',
+    'SE', 'SCSE', 'SCSE2', 'NonLocal', 'CBAM', 'MCBAM', 'EMA', 'ECA', 'AECA', 'TPA',
+    'ResLayerZH'
 ]
