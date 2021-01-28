7d58b5dcabf18f19bac03dee95db2d34978b12a4
zhangshuai <zhangshuai@ainnovation.com>
2021-01-28 11:45:07 +0800

    anjie

---
+++ configs/pspnet/anjie.py
@@ -0,0 +1,203 @@
+# ======================================= model settings ======================================= #
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+num_classes = 4
+dropout_ratio=0.5
+dupsample=dict(scale=8)
+# dupsample=None
+attention_cfg=dict(type='TPA')
+# attention_cfg=None
+align_corners=False
+model = dict(
+    type='EncoderDecoder',
+    pretrained='https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    backbone=dict(
+        type='ResNet',
+        depth=18,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        dilations=(1, 1, 2, 4), # (1, 1, 1, 1)
+        strides=(1, 2, 1, 1),
+        norm_cfg=norm_cfg,
+        norm_eval=False,
+        style='pytorch',
+        contract_dilation=True
+    ),
+    decode_head=dict(
+        type='PSPHead',
+        in_channels=512,
+        in_index=3,
+        channels=128, # 256
+        dropout_ratio=dropout_ratio,
+        num_classes=num_classes,
+        norm_cfg=norm_cfg,
+        align_corners=align_corners,
+        dupsample=dupsample,
+        ppm_cfg=dict(ppm_channels=128,
+                     pool_scales=(2, 4, 8, 16),
+                     pooling='mix',
+                     attention_cfg=attention_cfg),
+        loss_decode=[dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+                     dict(type='DiceLoss', loss_weight=2.0),
+                     ]
+    ),
+    auxiliary_head=dict(
+        type='FCNHead',
+        in_channels=256,
+        in_index=2,
+        channels=128,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=dropout_ratio,
+        num_classes=num_classes,
+        norm_cfg=norm_cfg,
+        align_corners=align_corners,
+        dupsample=dupsample,
+        loss_decode=[dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4),
+                     dict(type='DiceLoss', loss_weight=0.4),
+                     ]
+    ),
+)
+warmup_du_cfg = dict(
+    interval=10,
+    optimizer=dict(type='Ranger', lr=0.01),
+    # optimizer=dict(type='Adamax', lr=0.01, weight_decay=0.0005),
+    total_runs=0,
+    by_epoch=True)
+# model training and testing settings
+train_cfg = dict()
+test_cfg = dict(mode='whole')
+
+# ======================================= dataset settings ======================================= #
+data_root = '/root/public02/manuag/zhangshuai/data/anjie'
+dataset_type = 'AnjieDataset'
+dataset = 'dataset/anjie0122_split_unq'
+classes = ['background', 'huahen', 'zangwu', 'laji']
+labels = [0, 1, 2, 3]
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+img_scale=(1024, 1024)
+crop_size = (1024, 1024)
+# img_scale=(256, 256)
+# crop_size = (256, 256)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', reduce_zero_label=False),
+    dict(type='Relabel', labels=labels),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='MVResize', h_range=(0.8, 1.2), w_range=(0.8, 1.2), keep_ratio=True),
+    dict(type='MVRotate', angle_range=(-45, 45), center=None),
+    dict(type='MVCrop', crop_size=crop_size, crop_mode='random',
+         pad_mode=["range", "constant"], pad_fill=[[0, 255], 0], pad_expand=1.2),
+    dict(type='PhotoMetricDistortion',
+         brightness_delta=50,
+         contrast_range=(0.8, 1.2),
+         saturation_range=(0.8, 1.2),
+         hue_delta=50),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+val_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', reduce_zero_label=False),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=img_scale,
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=False,
+        transforms=[
+            dict(type='Relabel', labels=labels),
+            dict(type='MVCrop', crop_size=crop_size, crop_mode='center',
+                 pad_mode=['constant', 'constant'], pad_fill=[0, 0], pad_expand=1.0),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+        ])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=img_scale,
+        flip=False,
+        transforms=[
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+data = dict(
+    samples_per_gpu=16,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        dataset=dataset,
+        data_root=data_root,
+        classes=classes,
+        labels=labels,
+        split='train',
+        test_mode=False,
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        dataset=dataset,
+        data_root=data_root,
+        classes=classes,
+        labels=labels,
+        split='val',
+        test_mode=False,
+        pipeline=val_pipeline),
+    test=dict(
+        type=dataset_type,
+        dataset=dataset,
+        data_root=data_root,
+        classes=classes,
+        split='test',
+        test_mode=True,
+        pipeline=test_pipeline),)
+
+# ======================================= runtime settings ======================================= #
+log_config = dict(
+   interval=1,
+   hooks=[
+       dict(type='StatisticTextLoggerHook', by_epoch=True),
+       #dict(type='TensorboardLoggerHook')
+   ])
+seed = 4
+deterministic = None
+cudnn_benchmark = False
+gpu_ids = [1]
+
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+# workflow = [('train', 1), ('val', 1)]
+workflow = [('train', 1)]
+
+work_dir = data_root + '/' + dataset
+task_name = 'anjie_2021_0128'
+segmentor_type = 'manuvision'
+
+# ======================================= schedule settings ======================================= #
+optimizer = dict(type='Ranger', lr=0.01, weight_decay=0.0005,
+                 paramwise_cfg = dict(custom_keys={'backbone': dict(lr_mult=0.1)})) # , decay_mult=0.9
+optimizer_config = dict(type='Fp16OptimizerHook', loss_scale=512.0)
+# learning policy
+lr_config = dict(policy='CosineAnnealing', min_lr=2e-5, by_epoch=True,
+                 warmup='linear', warmup_iters=8, warmup_ratio=0.01,
+                 warmup_by_epoch=True)
+# runtime settings
+runner = dict(type='EpochBasedRunner', max_epochs=500)
+checkpoint_config = dict(by_epoch=True, interval=1, max_keep_ckpts=1, best_type='sum')
+evaluation = dict(interval=1, metric='mIoU', rescale=False, ignore_index=[0],
+                  f1_cfg=dict(com_f1=True, type='pix_iof', threshold=[0.2]*10,
+                              defect_filter=dict(type='box', size=[5, 5])),
+                  best_metrics = ['IoU', 'Acc', 'Recall', 'Precision', 'F1'])
+# TODO load best_metrics when resume
+
+# ======================================= convert ======================================= #
+convert_size = (1024, 1024)
---
+++ mmseg/datasets/anjie.py
@@ -0,0 +1,143 @@
+
+import time
+import random
+import os.path as osp
+
+from mmseg.datasets.builder import DATASETS
+from mmseg.datasets.ainno import AinnoDataset
+
+CLASSES = ['background', 'huahen', 'zangwu', 'laji']
+LABELS = [0, 1, 2, 3]
+PALETTE = [[0, 0, 0], [0, 0, 255], [255, 0, 0], [0, 255, 0]]
+
+@DATASETS.register_module()
+class AnjieDataset(AinnoDataset):
+    def __init__(self,
+                 classes=CLASSES,
+                 palette=PALETTE,
+                 labels=LABELS,
+                 **kwargs):
+        super(AnjieDataset, self).__init__(
+            classes=classes,
+            palette=palette,
+            labels=labels,
+            **kwargs)
+
+
+    def _get_split_file(self):
+        self._split_file = osp.join(self.data_root, '{}/{}.csv'.format(self.dataset, self.split))
+        if not osp.isfile(self._split_file):
+            raise ValueError('Unexist dataset _split_file: {}'.format(self._split_file))
+
+    def _pre_class_balance(self, max_times=10):
+        class_nums_str = ''
+        class_times_str = ''
+        sort_samples = []
+        ng_num = 0
+        for k, v in self.class_samples.items():
+            class_nums_str += '{}: {}, '.format(k, len(v))
+            sort_samples.append(v)
+            ng_num += len(v)
+        self.class_avg_num = int(ng_num / len(self.class_samples.keys()))
+        self.dataset_infos['class_nums'] = class_nums_str
+        # 按长度升序
+        sort_samples.sort(key=lambda x: len(x))
+        class_times = [self.class_avg_num / len(sort_samples[i]) for i in range(len(sort_samples))]
+        for t in class_times:
+            class_times_str += '{:.2f}, '.format(t)
+        self.dataset_infos['class_times'] = class_times_str
+        class_times = [min(round(t), max_times) for t in class_times] # round 4舍5入
+        self.major_smaples = sort_samples[-1]
+        self.few_samples = []
+        for i, t in enumerate(class_times[:-1]):
+            t = max(t+1, 1)
+            smaples = sort_samples[i] * t
+            self.few_samples += smaples[:]
+
+    def _epoch_balance(self, ):
+        self.ok_samples = random.sample(self.ok_ori_samples, self.ok_len)
+        random.shuffle(self.major_smaples)
+        self.ng_samples = self.few_samples + self.major_smaples[:self.class_avg_num]
+        self.img_infos = self.ok_samples + self.ng_samples
+        random.shuffle(self.img_infos)
+
+    def info2sample(self, line):
+        _image, _mask, label = line.rstrip('\n').split(',')[:3]
+        image_path = osp.join(self.data_root, _image)
+        mask_path = osp.join(self.data_root, _mask)
+        image_exist = self.check_image_exist(image_path)
+        mask_exist = self.check_image_exist(mask_path)
+        sample = None
+        if image_exist and mask_exist:
+            sample = dict(filename=image_path,
+                          label=label,
+                          ann=dict(seg_map=mask_path))
+        return sample
+
+    def update_samples(self, sample):
+        if not sample:
+            return
+        label = sample['label']
+        if self.split == 'train':
+            if label == 'ok' or label == 'OK':
+                self.ok_ori_samples.append(sample)
+            elif label not in self.class_samples.keys():
+                label_init = {label: [sample]}
+                self.class_samples.update(label_init)
+            elif label in self.class_samples.keys():
+                self.class_samples[label].append(sample)
+            else:
+                raise ValueError('Unkonwn label type !!!')
+        elif self.split == 'val':
+            if label == 'ok' or label == 'OK':
+                self.ok_ori_samples.append(sample)
+            else:
+                self.ng_ori_samples.append(sample)
+        else:
+            raise ValueError('Unkonwn dataset split : {}'.format(self.split))
+
+    def load_annotations(self, ):
+        self.ok_ori_samples = []
+        self.ng_ori_samples = []
+        self.class_samples = {}
+        self.total_index = 0
+
+        self._get_split_file()
+        if not osp.isfile(self._split_file):
+            raise ValueError('Unexist dataset _split_file: {}'.format(self._split_file))
+        with open(self._split_file, "r") as lines:
+            lines = list(lines)
+        for line in lines:
+            sample = self.info2sample(line)
+            self.update_samples(sample)
+
+        ##---------------- 训练集类别平衡 ----------------##
+        self.ok_ori_len = len(self.ok_ori_samples)
+        if self.split == 'train':
+            self.ok_len = int(self.ok_ori_len * 0.1)
+            self._pre_class_balance(max_times=10)
+            self._epoch_balance()
+            self.ng_len = len(self.ng_samples)
+        elif self.split == 'val':
+            self.ok_len = self.ok_ori_len
+            self.img_infos = self.ok_ori_samples + self.ng_ori_samples
+            self.ng_len = len(self.ng_ori_samples)
+
+        self.set_len = len(self.img_infos)
+        assert self.set_len == self.ok_len + self.ng_len
+        self.dataset_infos['sample_nums'] = '(OK: {}, NG: {} ,Total: {})'.format(
+            self.ok_len, self.ng_len, self.set_len)
+        time.sleep(1)
+
+    def epoch_ops(self):
+        """Some operations that need to be performed every n epochs. """
+        self._epoch_balance()
+
+    def __getitem__(self, idx):
+        if self.test_mode:
+            return self.prepare_test_img(idx)
+        else:
+            return self.prepare_train_img(idx)
+
+    def __len__(self):
+        return self.set_len
